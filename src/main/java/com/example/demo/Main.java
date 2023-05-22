d96d7cfd5f00ec1c61b19787ce834a67d3e14ad4
Gohan255 <117871093+Gohan255@users.noreply.github.com>
2023-05-22 20:41:34 +0100

    3

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -3,13 +3,6 @@ package com.example.demo;
 public class Main {
     public static void main(String[] args) {
 
-        String as = "asdas";
-        String asdas = "asdas";
-        String das = "asdas";
-        String fsad = "asdas";
-
-        int asd = 1;
-        int asd2 = 1;
 
     }
 }
